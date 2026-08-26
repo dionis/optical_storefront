9986af1c6874cf78395b4fd661b0a631d0736b7c
Eyewear Dev <dev@eyewear-store.local>
2026-08-26 18:26:13 -0400

    Update model upload fixed

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -3,7 +3,7 @@ set -e
 
 cd apps/vto-web
 npm install
-VITE_TRYON_BASE=/tryon-3d/ VITE_VISION_API_BASE=/medusa/vision-measure npm run build
+VITE_TRYON_BASE=/tryon-3d/ VITE_VISION_API_BASE=/medusa npm run build
 mkdir -p ../capri-storefront/public/tryon-3d
 cp -r dist/* ../capri-storefront/public/tryon-3d/
 
